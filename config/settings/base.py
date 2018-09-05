82bd8f9753fc79e083e41f57ca4749f4e4fa39e1
Luciane Araujo <luaraujo@luaraujo.com>
2018-09-05 03:29:44 -0300

    Forcing redeplooy

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,7 +83,7 @@ LOCAL_APPS = [
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
-INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS 
 
 # MIGRATIONS
 # ------------------------------------------------------------------------------
