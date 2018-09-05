b23742badeb1950c2873cd1d431a46a8d6cf1edd
Luciane Araujo <luaraujo@luaraujo.com>
2018-09-05 04:26:27 -0300

    Trying to solve static files issue

--- config/urls.py
+++ config/urls.py
@@ -28,8 +28,6 @@ urlpatterns = [
     ),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-) + static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT
 )
 
 if settings.DEBUG:
