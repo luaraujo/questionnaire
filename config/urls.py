31ff89766262a06d1f112fc8653a0859c1f9879a
Luciane Araujo <luaraujo@luaraujo.com>
2018-09-05 03:54:23 -0300

    Trying to solve static files issue

--- config/urls.py
+++ config/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     ),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+) + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
 )
 
 if settings.DEBUG:
@@ -55,8 +57,3 @@ if settings.DEBUG:
         import debug_toolbar
 
         urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
-
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
